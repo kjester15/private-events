b219d557029cd8e9d8eed5266c28df41d2610c5f
Kyle Jester <95105911+kjester15@users.noreply.github.com>
2024-02-29 15:13:15 -0800

    Create working route for see event link for each event

--- app/controllers/attended_event_controller.rb
+++ app/controllers/attended_event_controller.rb
@@ -1,6 +1,21 @@
 class AttendedEventController < ApplicationController
   def show
-    # @attended_event = AttendedEvent.find(params[:id])
-    # @attendees = AttendedEvent.where("event_id = ?", params[:id]).attendee_id.all
+    @attended_event = AttendedEvent.where("attended_event_id = ?", params[:id])
+    @events = Event.where("id = ?", params[:id])
+    # @attendees = User.where("id = ?", params[:id])
+  end
+
+  def new
+    @attended_event = AttendedEvent.new
+  end
+
+  def create
+    @attended_event = AttendedEvent.new(attendee_id: current_user.id, attended_event_id: params[:id])
+
+    if @attended_event.save
+      redirect_to events_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 end
