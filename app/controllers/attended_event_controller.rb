8461c8a551409653ff679abd22a6b752535d03d9
Kyle Jester <95105911+kjester15@users.noreply.github.com>
2024-02-28 16:51:30 -0800

    Add attended_event routes, create attended_event show view, and add link_to attended_events show view to index

--- app/controllers/attended_event_controller.rb
+++ app/controllers/attended_event_controller.rb
@@ -1,2 +1,6 @@
 class AttendedEventController < ApplicationController
+  def show
+    # @attended_event = AttendedEvent.where("event_id = ?", params[:id])
+    # @attendees = AttendedEvent.where("event_id = ?", params[:id]).attendee_id.all
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :events, foreign_key: "creator_id", class_name: "Event"
-  has_and_belongs_to_many :events, foreign_key: "attendee_id", class_name: "Event"
+  has_many :attended_events, foreign_key: "attendee_id", class_name: "Event", through: :attended_event
 end
