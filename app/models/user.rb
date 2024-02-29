377c8f4eca9e4984692f6f235abf98d3b6f338fd
Kyle Jester <95105911+kjester15@users.noreply.github.com>
2024-02-28 17:24:44 -0800

    Generate devise views and add first_name and last_name to sign up form

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  # attr_accessor :first_name, :last_name
   has_many :events, foreign_key: "creator_id", class_name: "Event"
   has_many :attended_events, foreign_key: "attendee_id", class_name: "Event", through: :attended_event
 end
