ef208966a5a1e364b8d5802ed7937c2a9612b247
Kyle Jester <95105911+kjester15@users.noreply.github.com>
2024-03-06 16:18:07 -0800

    Fix attended_events controller name and routing to allow attend button to work

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:show]
   resources :events
-  resources :attended_event
+  resources :attended_events
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  get 'up' => 'rails/health#show', as: :rails_health_check
   get 'profile', to: 'users#show'
   # Defines the root path route ("/")
-  root "events#index"
+  root 'events#index'
 end
