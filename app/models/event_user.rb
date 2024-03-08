5643917be29b5c27a19f447fd8d8284538d563c8
Kyle Jester <95105911+kjester15@users.noreply.github.com>
2024-03-08 15:37:03 -0800

    Add validation to EventUser model to limit attendee to attend event only once

--- app/models/event_user.rb
+++ app/models/event_user.rb
@@ -1,4 +1,6 @@
 class EventUser < ApplicationRecord
+  validates :attendee_id, uniqueness: { scope: :attended_event }
+
   belongs_to :attendee, class_name: "User"
   belongs_to :attended_event, class_name: "Event"
 end
