fdad614d8d41f68fd9adef04411d044dc0517b93
Kyle Jester <95105911+kjester15@users.noreply.github.com>
2024-03-09 16:18:30 -0800

    Create #past and #upcoming in Event model and split index view into past and upcoming

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,12 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: "User"
   has_many :event_users, foreign_key: "attended_event_id"
   has_many :attendees, through: :event_users, source: :attendee
+
+  def past
+    @past = Event.where("date < :today", { today: Date.today }).order(date: :asc)
+  end
+
+  def upcoming
+    @upcoming = Event.where("date >= :today", { today: Date.today }).order(date: :asc)
+  end
 end
