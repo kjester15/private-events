ebd83fcbbeebb7d845cfc39878c1f680f4344a9c
Kyle Jester <95105911+kjester15@users.noreply.github.com>
2024-03-09 18:38:56 -0800

    Add user authentication to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, only: [:show]
+
   def show
     @events = Event.where("creator_id = ?", current_user.id).order(date: :asc)
     @attendances = current_user.attended_events
