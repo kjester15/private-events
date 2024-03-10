4ae1b3ab802a26ac6babe97ae038b6b5ab3f5e0d
Kyle Jester <95105911+kjester15@users.noreply.github.com>
2024-03-09 17:37:08 -0800

    Create #destroy in Events controller and add appropriate elements to views/UI

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -38,6 +38,12 @@ class EventsController < ApplicationController
     end
   end
 
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+    redirect_to profile_path
+  end
+
   private
 
   def event_params
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: "User"
-  has_many :event_users, foreign_key: "attended_event_id"
+  has_many :event_users, foreign_key: "attended_event_id", dependent: :destroy
   has_many :attendees, through: :event_users, source: :attendee
 
   scope :past, -> { where("date < :today", { today: Date.today }) }
